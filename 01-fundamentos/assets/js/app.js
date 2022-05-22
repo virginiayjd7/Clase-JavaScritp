4124a9ddf92c03febd9440ee1160e045998821cf
Virginia Aquino <37305214+virginiayjd7@users.noreply.github.com>
2022-05-22 13:59:32 -0500

    initial commit

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,28 @@
+//console.log('hola mundo');
+// let a = 10; // nueva
+// let b = 10;
+// let c = 10;
+// let d = 10;
+// let x = a + b;
+alert('Hola des app.js');
+let a = 10, // nueva
+    b = 60,
+    c = 'hola ',
+    d = 'apiderman',
+    x = a + b;
+const saludo = c + d;
+c = 'hola mudno';
+console.table({ a, b, c, d, x })
+    // console.log('a', a)
+    // console.log('b', b)
+    // console.log('c', c)
+    // console.log('%c mis valores ', 'color:blue')
+    // console.log({ a });
+    // console.log({ b });
+    // console.log({ c });
+    // var b = 10; //antigua
+    // const c = 10; //
+    //c = 20; no se puede hcer
+
+// a = 20;
+// b = 20;
